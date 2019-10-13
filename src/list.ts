6da5bdf8e3dba06c2fcb93e70538c8d971959250
Garrett van Wageningen <ghvanw283@gmail.com>
2019-10-12 21:39:09 -0500

    different attempt

--- src/list.ts
+++ src/list.ts
@@ -1,63 +1,49 @@
-
-export interface Node<A> {
-    data: A;
-    next: Node<A> | null;
+interface Node<A> {
+    data: A,
+    next: List<A> | null
 }
 
 export class List<A> implements Iterable<A> {
     private root: Node<A> | null;
-    private last: Node<A> | null; // last will maintain a pointer to the virtual but potentially not actual end of the list; 
 
-    constructor(root: Node<A> | null = null) {
-        this.root = root;
-        this.last = root === null ? null : this.findLast(root); 
+    private constructor(node?: Node<A>) {
+        this.root = node === undefined ? null : node;
+    }
+    
+    static empty<A>(): List<A> {
+        return new List();
+    }
+
+    static from<A>(data: A): List<A> {
+        return new List({ data: data, next: null });
+    }
+
+    static new<A>(data: A, next: List<A>): List<A> {
+        return new List({ data: data, next: next });
     }
 
     isEmpty(): boolean {
         return this.root === null;
     }
 
-    count(): number {
-        return this.findLength(this.root, 0);
-    }
-
-    concat(other: List<A>): List<A> {
-        if (this.root === null) {
-            return new List(other.root);
-        }
-
-        this.last!.next = other.root;
-
-        return new List(this.root);
+    cons(data: A): List<A> {
+        return new List({ data: data, next: this });       
     }
 
     headTail(): [A | null, List<A> | null] {
-        const head = this.root && this.root.data; // refactor to this.root?.data when 3.7 lands
-        const tail = this.root && this.root.next; // refactor to this.root?.next when 3.7 lands
-        return [head, new List(tail)];
+        return [this.root && this.root.data, this.root && this.root.next];
     }
 
     *[Symbol.iterator](): IterableIterator<A> {
-        let nextNode = this.root;
+        let [head, tail] = this.headTail();
 
-        while (nextNode !== null) {
-            yield nextNode.data;
+        while (head !== null && tail !== null) {
+            yield head;
+            [head, tail] = tail.headTail();
+
+            if (tail === null && head !== null) {
+                yield head;
+            }
         }
-    } 
-
-    private findLast(node: Node<A>): Node<A> {
-        if (node.next === null) {
-            return node;
-        }
-
-        return this.findLast(node.next);
-    }
-
-    private findLength(node: Node<A> | null, total: number): number {
-        if (node === null) {
-            return total;
-        }
-
-        return this.findLength(node.next, total + 1);
     }
 }
